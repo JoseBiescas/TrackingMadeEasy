2400e6a828d692c2c23fa31abf957e5fbd7d4607
Jose Biescas <jose.biescas@upr.edu>
2020-09-04 22:28:46 -0400

    Linked priv routes, dashboard, and auth user (or expired) to main App.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import "./App.css";
 
 import { Provider } from "react-redux";
@@ -10,7 +13,29 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/authentication/Register";
 import Login from "./components/authentication/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/private-route/PrivateRoute";
 
+// Check for token to keep user logged in 
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  //De code token and get user info and exp
+  const decoded = jwt_decode(token);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //logout
+    store.dispatch(logoutUser());
+
+    //redirect to login
+    window.location.href = "./login";
+  }
+}
 class App extends Component {
   render() {
     return (
@@ -21,6 +46,9 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
           </div>
         </Router>
       </Provider>
