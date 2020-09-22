b59c3bf50f59d988b2c0bdaba7cbc00d79a152f4
Jose Biescas <jose.biescas@upr.edu>
2020-09-22 19:55:17 -0400

    Decorated cards a bit.

--- client/src/components/cards/ViewCard.js
+++ client/src/components/cards/ViewCard.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getCards, deleteCard } from "../../actions/cardAction";
 
+import "./ViewCard.css";
+
 class ViewCard extends Component {
-  
   componentDidMount() {
     if (!this.props.auth.isAuthenticated) {
       this.props.history.push("/login");
@@ -12,32 +13,36 @@ class ViewCard extends Component {
   }
 
   onDelete = (e) => {
-      e.preventDefault();
-      this.props.deleteCard(e.target.value);
-  }
+    e.preventDefault();
+    this.props.deleteCard(e.target.value);
+  };
 
   render() {
-      let cardList = this.props.cards.map(card => (
-          <div className="card" key={card._id}>
-            <div className="delete-btn" >
-              <button
-                className="btn btn-danger red waves-effect"
-                value= {card._id}
-                onClick={this.onDelete}
-              >
-                Delete
-              </button>
-            </div>
-            <h3>{card.title}</h3>
-            <p>{card.description}</p>
-          </div>
-      ))
-      return (
-          <div className="container">
-            <h1>Cards</h1>
-            {cardList}
-          </div>
-      )
+    let cardList = this.props.cards.map((card) => (
+      <div className="card" key={card._id}>
+        <div className="delete-btn">
+          <button
+            className="btn btn-danger red waves-effect"
+            value={card._id}
+            onClick={this.onDelete}
+          >
+            X
+          </button>
+        </div>
+        <div className="title">
+          <h3>{card.title}</h3>
+        </div>
+        <div className="description">
+          <p>{card.description}</p>
+        </div>
+      </div>
+    ));
+    return (
+      <div className="container">
+        <h1>Cards</h1>
+        {cardList}
+      </div>
+    );
   }
 }
 
@@ -45,7 +50,7 @@ class ViewCard extends Component {
 const mapStateToProps = (state) => ({
   auth: state.auth,
   errors: state.errors,
-  cards: state.cards.cards // name: state.cardReducerName in root.cardReducer
+  cards: state.cards.cards, // name: state.cardReducerName in root.cardReducer
 });
 
 // export default connect(mapStateToProps, { getCards })(withRouter(ViewCard));
