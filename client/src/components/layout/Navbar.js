038c775d909e56ab0a16b24abe6e4244dd201a39
jose.biescas <jose.biescas@upr.edu>
2021-05-04 18:14:08 -0400

    Added Logo to navbar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,23 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from "../../images/new-logo.png";
+import { Box, Flex } from "@chakra-ui/react";
 import "./Navbar.css";
 
 class Navbar extends Component {
   //z-depth-0
   render() {
     return (
-      <div className="navbar">
-        <nav className="navbar-depth"> 
-          <div className="navbar-wrapper">
-            <Link
-              to="/"
-              className="navbar-logo"
-            >
-              <b>TRACKING MADE EASY</b>
-            </Link>
-          </div>
-        </nav>
-      </div>
+      <Flex
+        w="100%"
+        h="90px"
+        bg="#80cbc4"
+        flexDir="row"
+        justify="space-between"
+      >
+        <Box paddingLeft="40px" paddingTop="7px">
+          <img src={logo} />
+        </Box>
+        <Box fontSize="xxx-large" color="black" fontFamily="sans-serif">
+          <Link to="/">
+            <b>TRACKING MADE EASY</b>
+          </Link>
+        </Box>
+        <Box paddingRight="40px" paddingTop="7px">
+          <img src={logo} />
+        </Box>
+      </Flex>
     );
   }
 }
